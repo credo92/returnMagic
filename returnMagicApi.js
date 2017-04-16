6b93bbacd79ee7646f85a0d6d71d510bc5205351
Vipul Srivastav <vipulsrivastav92@gmail.com>
2017-04-16 14:51:33 -0400

    Shippo API Task done

---
+++ returnMagicApi.js
@@ -0,0 +1,186 @@
+/* Problems faced
+  - used postman to test rest calls
+  - i am not sure about the ES6 class structure, whether did i
+    used the concept well
+  - problem using lodash , mocha , joi ( validation of postdata INPUT)
+*/
+// constructor(token)
+// createData(path,postData)
+// generic REST APi PUT CALL for address, parcels, customs,SHIPMENT
+// getRates creates shipment and prints the rates array
+
+const https = require('https');
+
+class ProviderAbstract
+{
+  constructor(token)
+  {
+    // To be implemented
+    this.token = token;
+  }
+
+  createData(path,postData){
+    // this.token = token;
+    this.path = path;
+    this.postData = postData;
+    const shippoToken = this.token // Replace with your token
+
+    let postOptions = {
+    	host: 'api.goshippo.com',
+    	port: 443,
+    	path: path,
+    	method: 'POST',
+    	headers: {
+    		'Authorization': shippoToken,
+    		'Content-Type': 'application/json'
+    	}
+    };
+
+    const postReq = https.request(postOptions, (res)=>{
+    	res.setEncoding('utf8');
+    	res.on('data', (chunk)=>{
+    		console.log(JSON.parse(chunk,null,2));
+    	})
+    });
+    postReq.write(JSON.stringify(postData));
+    postReq.end();
+  }
+
+  getRates(path,postData)
+    {
+
+    this.path = path;
+    this.postData = postData;
+    const shippoToken = this.token // Replace with your token
+
+    let postOptions = {
+    	host: 'api.goshippo.com',
+    	port: 443,
+    	path: path,
+    	method: 'POST',
+    	headers: {
+    		'Authorization': shippoToken,
+    		'Content-Type': 'application/json'
+    	}
+    };
+
+    const postReq = https.request(postOptions, (res)=>{
+    	res.setEncoding('utf8');
+    	res.on('data', (chunk)=>{
+    		console.log(JSON.parse(chunk,null,2));
+    	})
+    });
+    postReq.write(JSON.stringify(postData));
+    postReq.end();
+
+
+    }
+
+}
+class CanadaPostProvider extends ProviderAbstract
+{
+  // To be implemented
+
+  getRates(path,postData)
+    {
+
+    this.path = path;
+    this.postData = postData;
+    const shippoToken = this.token // Replace with your token
+
+    let postOptions = {
+     host: 'api.goshippo.com',
+     port: 443,
+     path: path,
+     method: 'POST',
+     headers: {
+       'Authorization': shippoToken,
+       'Content-Type': 'application/json'
+     }
+    };
+
+    const postReq = https.request(postOptions, (res)=>{
+     res.setEncoding('utf8');
+     res.on('data', (chunk)=>{
+  console.log(JSON.parse(chunk,null,2));
+
+     })
+    });
+    postReq.write(JSON.stringify(postData));
+    postReq.end();
+
+
+    }
+  }
+
+const myCPInstance = new CanadaPostProvider('ShippoToken shippo_test_f2a949829c09df2740bf198b87dd5548aa02943b')
+  var addressFrom={
+                      "name": "Vipul Srivastav",
+                      "company": "Belfort 103",
+                      "street_no": "",
+                      "street1": "Chemin de la Cote-des-neiges",
+                      "street2": "Chemin de la Côte-des-Neiges",
+                      "city": "Montreal",
+                      "state": "QC",
+                      "zip": "H3H 1V8",
+                      "country": "CA",
+                      "phone": "512-123-1234",
+                      "email": "vipulsrivastav92@gmail.com"
+                    };
+var addressTo=		{
+										  "name":"Shawn Ippotle",
+									    "company":"Shippo",
+									    "street1":"215 Clayton St.",
+									    "city":"San Francisco",
+									    "state":"CA",
+									    "zip":"94117",
+									    "country":"US", // iso2 country code
+									    "phone":"+1 123 345 9350",
+									    "email":"shippotle@goshippo.com"
+										};
+var parcel =       {
+                      "length": "5",
+                          "width": "5",
+                          "height": "5",
+                          "distance_unit": "in",
+                          "weight": "2",
+                          "mass_unit": "lb",
+                     };
+var customs_declaration =  {
+                        "contents_type": "MERCHANDISE",
+                        "contents_explanation": "T-Shirt purchase",
+                        "non_delivery_option": "RETURN",
+                        "certify": true,
+                        "certify_signer": "Vipul Srivastav",
+                        "items": [{
+                       				     "description":"T-Shirt",
+                       				     "quantity":20,
+                       				     "net_weight":"1",
+                       				     "mass_unit":"lb",
+                       				     "value_amount":"200",
+                       				     "value_currency":"CAD",
+                       				     "origin_country":"CA",
+                        			}]
+                          };
+const myAddress = {
+  "address_from": addressFrom,
+	 "address_to": addressTo,
+	 "parcels": parcel,
+	 "customs_declaration": customs_declaration,
+
+   //To get Canada Post Object Id for carrier_accounts we can
+  //used endpoint https://api.goshippo.com/carrier_accounts/
+
+	 "carrier_accounts": ['8f17d003322b42dd90fd8437927b0511'],
+	 "async": true
+} ;// Structure of the object to be defined
+
+// Following POST calls create address, parcels, customs, then we get Rates by
+myCPInstance.createData('/addresses',addressFrom);
+myCPInstance.createData('/addresses',addressTo);
+
+myCPInstance.createData('/parcels',parcels);
+
+myCPInstance.createData('/customs/declarations/',customs_declaration);
+
+ myCPInstance.getRates('/shipments',myAddress);
